8f9187c1bf3c77d57cd093d9cf29060766ddf4f7
Martin C <martin.czabadaj@gmail.com>
2020-01-31 12:23:05 +0100

    new save model

--- models/Save.js
+++ models/Save.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const SaveSchema = new Schema(
+const SaveSchema = new Schema({
+  user: Schema.Types.ObjectId,
+  funds: { type: Number, required: true },
+  favorites: [
     {
-       user: Schema.Types.ObjectId,
-       funds: {type: Number, required: true},
-       favorites: [{type: String, required: true}],
-       owned: [{
-           symbol: {type: String, required: true},
-           quantity: {type: Number, required: true}
-       }]
+      symbol: { type: String, required: true },
+      customName: { type: String, required: false }
     }
-);
+  ],
+  owned: [
+    {
+      symbol: { type: String, required: true },
+      quantity: { type: Number, required: true },
+      customName: { type: String, required: false }
+    }
+  ]
+});
 
-module.exports = mongoose.model('Save', SaveSchema);
+module.exports = mongoose.model("Save", SaveSchema);
